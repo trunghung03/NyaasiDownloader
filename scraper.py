ec8f39eb7ccc4a96c99ee32f82f5699e3dcd94e6
trunghung03 <nguyentrunghung03@gmail.com>
2020-04-05 10:48:50 +0700

    name variables on top for easier customization

--- scraper.py
+++ scraper.py
@@ -9,17 +9,19 @@ import time
 
 
 folder = '/home/synapse26/Downloads/FAKKU/'  # change this folder to your destination folder to check for duplicates
-fakkuHTML = requests.get('https://sukebei.nyaa.si/user/rbot2000') # change this part if you want to download from another user
+torrent_name = 'fakku' # change this to your wanted torrent to check for duplicates
+url = 'https://sukebei.nyaa.si' # use 'https://nyaa.si' if you prefer the vanilla
+fakkuHTML = requests.get(url + '/user/rbot2000') # change this part if you want to download from another user
 fakkuHTML.raise_for_status()
 fakkuSoup = bs4.BeautifulSoup(fakkuHTML.text, features='lxml')
-fakkuLinks = fakkuSoup.select("tbody > tr > td > a")
+fakkuLinks = fakkuSoup.select('tbody > tr > td > a')
 
 
 def checklink(links): # check for torrents that you want to get
 	linkslist = []
 	for link in links:
 		text = link.getText().lower()
-		if not text.startswith("fakku") or os.path.isdir(folder + text):
+		if not text.startswith(torrent_name) or os.path.isdir(folder + text):
 		# change this part if you want to download a series from another user
 			continue
 		linkslist.append(link)
@@ -53,10 +55,10 @@ def findthelinks(dalist): # find all download link from final list
 
 def downloaddalist(downloadlist): # download the torrent
 	for download in downloadlist:
-		webbrowser.open('https://sukebei.nyaa.si' + download)
+		webbrowser.open(url + download)
 		time.sleep(1)
 
 
 fakku = checklink(fakkuLinks)
-finalList = listmaker(resultsetlist(fakkuSoup.select("tbody > tr")), fakku)
+finalList = listmaker(resultsetlist(fakkuSoup.select('tbody > tr')), fakku)
 downloaddalist(findthelinks(finalList))
